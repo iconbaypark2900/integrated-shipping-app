185c4b6a701ef61a89d3033e387d7363ddd8e8ad
iconbaypark2900 <jonaston015@gmail.com>
2023-02-05 17:20:05 -0500

    ,

---
+++ web/shopify.js
@@ -0,0 +1,33 @@
+import { join } from "path";
+import { QRCodesDB } from "./qr-codes-db.js";
+import { BillingInterval, LATEST_API_VERSION } from "@shopify/shopify-api";
+import { shopifyApp } from "@shopify/shopify-app-express";
+import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
+let { restResources } = await import(
+  `@shopify/shopify-api/rest/admin/${LATEST_API_VERSION}`
+);
+// If you want IntelliSense for the rest resources, you should import them directly
+// import { restResources } from "@shopify/shopify-api/rest/admin/2022-10";
+
+const dbFile = join(process.cwd(), "database.sqlite");
+const sessionDb = new SQLiteSessionStorage(dbFile);
+// Initialize SQLite DB
+QRCodesDB.db = sessionDb.db;
+QRCodesDB.init();
+
+const shopify = shopifyApp({
+  api: {
+    restResources,
+  },
+  auth: {
+    path: "/api/auth",
+    callbackPath: "/api/auth/callback",
+  },
+  webhooks: {
+    path: "/api/webhooks",
+  },
+  sessionStorage: sessionDb,
+});
+
+
+export default shopify;
